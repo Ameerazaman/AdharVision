26d83c93a32ed369337bc622d0587ebb861beb9c
Fathimath Ameera <fathimathameeraap2gmail.com>
2025-03-23 13:19:07 +0530

    fixed error in frondend url

--- backend/Src/App.ts
+++ backend/Src/App.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // app.use(cookieParser());
 const corsOptions = {
-    origin: ["http://localhost:3000","https://adhar-vision.vercel.app/"],
+    origin: ["http://localhost:3000","https://adhar-vision.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
